6dee2b67a3d731aab4e0aeadb8454c2b209ec739
Wan Aqim <wanaqim@gmail.com>
2023-12-04 10:39:18 +0800

    initial commit

---
+++ dict_practice.py
@@ -0,0 +1,10 @@
+where_my_things_are = {
+    'keys': 'in the drawer',
+    'phone': 'on the desk',
+    'glasses': 'next to the bed',
+    'wallet': 'in the backpack'
+}
+
+# Loop through the dictionary
+for k, v in where_my_things_are.items():
+    print(f"My {k} is kept {v}")
